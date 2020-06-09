f619b94161b050ab5fde03fc961906abefbfde16
venkatesh0311 <59863212+venkatesh0311@users.noreply.github.com>
2020-06-09 12:44:25 +0530

    Updates

---
+++ precache-manifest.8acb0368333b4da4946ee70494ade1de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04f8b7e56a9a0b319066fb3761fc228",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c14cae0fb49ebbbf45f4",
+    "url": "/monsters-rolodex/static/css/main.85b55cd6.chunk.css"
+  },
+  {
+    "revision": "beac283dd30f89fb9c8e",
+    "url": "/monsters-rolodex/static/js/2.56233d6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.56233d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14cae0fb49ebbbf45f4",
+    "url": "/monsters-rolodex/static/js/main.e1fdb1c4.chunk.js"
+  },
+  {
+    "revision": "bbe7a1588439bb95d353",
+    "url": "/monsters-rolodex/static/js/runtime-main.a7096cf4.js"
+  }
+]);
